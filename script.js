7a46bca300f2304f7b0964be94c2f75df2ecce35
Jacqueline <jacqueline.olsson@lerniastudent.se>
2026-09-18 21:39:25 +0200

    knapp och text fixad

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Hämta knapp elementet
+const button = document.getElementById("myBtn");
 
+// Skapa funktionen som gör kanppen klickbar
+function clickButton() {
+    document.getElementById("title").textContent =("Goodbye World.");
+}
+
+// Lägg till en eventListener(metod) som lyssnar på klickhändelsen(elementet)
+button.addEventListener("click", clickButton);
